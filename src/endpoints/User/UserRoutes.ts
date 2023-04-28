532c163cf0ca97e9553b3dd5d655e27a6215db81
sophiacorell <sophiacorell@tu-berlin.de>
2023-04-28 20:22:01 +0200

    registration -> Controller und Routes

---
+++ src/endpoints/User/UserRoutes.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import UserController from "../User/UserController"
+
+const router = express.Router();
+
+// POST request um user zu registrieren
+router.post("/user/registration", UserController.registration);
