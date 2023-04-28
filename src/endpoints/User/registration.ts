22fcbf66d2f3b4d34b587ce93bb266d053e986d1
sophiacorell <sophiacorell@tu-berlin.de>
2023-04-28 14:17:59 +0200

    registration kommentare hinzugefügt

--- src/endpoints/User/registration.ts
+++ src/endpoints/User/registration.ts
@@ -4,23 +4,30 @@ import bcrypt from "bcryptjs"
 const app = express();
 
 
-//User endpunkt definieren, keine ahnung ob das mit dem api/register klappt
-app.post("/api/register", async (req: Request, res: Response) =>{
+//User endpunkt definieren, keine ahnung ob das mit dem user/register klappt
+app.post("/user/registration", async (req: Request, res: Response) =>{
 
     //daten aus request extrahieren
     const { username, email, password, isAdmin, isOrganizer } = req.body;
 
     //prüfen ob daten vollständig
     if( !username || !email || !password){
-        //wenn nicht --> response
         return res.status(400).json({ message: "Please provide all required fields" });
     }
+    
+    //prüfen ob daten korrekt
+    //if( username bereits in datenbank ){
+    //  return res.status(400).json({ message: "Username is already taken" });
+    //}
 
     //paswort hashen
     const hashedPassword = await bcrypt.hash(password, 10);
 
     //user ertsellen und daten darin speichern
     const user = {username, email, hashedPassword, isAdmin, isOrganizer};
+
+    //user in datenbank speichern
+    //db.collection.save(user);   ???? keine ahnung
 
     //response dass user erstellt wurde
     res.status(201).json({message: "User registered successfully"});
