2edc209fe6d637a220f1efa668c67078b844bf7c
sophiacorell <sophiacorell@tu-berlin.de>
2023-05-05 18:00:23 +0200

    irgendwas seit dem letzten mal keine ahnung

--- src/endpoints/ShoppingCart/ShoppingCartController.ts
+++ src/endpoints/ShoppingCart/ShoppingCartController.ts
@@ -5,7 +5,10 @@ import ShoppingCartModel from './ShoppingCartModel';
 const add = async (req: Request, res: Response) => {
     //@ts-ignore
     ShoppingCartModel.findOne({creatorId: req.body.user.id}).then((data) => {
-        
+        if(!data){
+            throw new Error("keine ShoppingCart  mit creatorId gefunden")
+        }
+        data.items.push()
         return res.status(200).json(data);
     }).catch((err) => {
         return res.status(500).json({message: err.message});
