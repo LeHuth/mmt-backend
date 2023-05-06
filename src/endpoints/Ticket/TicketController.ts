9baaf3cc84dbf29cf63e23d3e9a9a2e299ff4d28
sophiacorell <sophiacorell@tu-berlin.de>
2023-05-06 13:59:10 +0200

    TicketController aber nur create und ka ob richtig

---
+++ src/endpoints/Ticket/TicketController.ts
@@ -0,0 +1,12 @@
+import {Request, Response } from "express";
+import TicketModel from "./TicketModel";
+import { Document } from "mongoose"
+
+const create = (req: Request, res: Response) => {
+    TicketModel.create(req.body)
+        .then((data) => {
+            return res.status(201).json(data);})
+        .catch((err) => {
+            return res.status(500).json({message: err.message});
+        });
+}
