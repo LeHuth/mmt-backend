fe2639b43033cd6869b0ea49722513a85f774b30
s60479 <s60479@bht-berlin.de>
2023-06-22 14:17:56 +0200

    ticket validation controller implemented

--- src/endpoints/Ticket/TicketController.ts
+++ src/endpoints/Ticket/TicketController.ts
@@ -27,6 +27,26 @@ const deleteById = (req:Request, res:Response) => {
     });
 }
 
+const validate = (req: Request, res: Response) => {
+    TicketModel.findOne({ uuid: req.params.uuid }).then((ticket) => {
+        if (!ticket) {
+          return res.status(404).json({ success: false, message: "Ticket nicht gefunden." });
+        }
+        if (ticket.isUsed) {
+          return res.status(409).json({ success: false, message: "Ticket ist bereits verwendet." });
+        }
+        ticket.isUsed = true;
+        ticket.save()
+          .then(() => {
+            return res.status(200).json({ success: true, message: "Ticket validiert." });
+          })
+          .catch((err) => {
+            return res.status(500).json({ success: false, message: err.message });
+          });
+      })
+      .catch((err) => {
+        return res.status(500).json({ success: false, message: err.message });
+      });
+  };
 
-
-export default { create, get, deleteById };
+export default { create, get, deleteById, validate };
