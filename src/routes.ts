3c0876ed9d174a3e77fe9d5415ef213549dce052
s60479 <s60479@bht-berlin.de>
2023-06-22 14:19:02 +0200

    ticket route added

--- src/routes.ts
+++ src/routes.ts
@@ -5,11 +5,13 @@ import EventRoutes from "./endpoints/Event/EventRoutes";
 import EventLocationRoutes from "./endpoints/EventLocation/EventLocationRoutes";
 import UserRoutes from "./endpoints/User/UserRoutes";
 import PaymentRoutes from "./endpoints/Payment/PaymentRoutes";
+import TicketRoutes from "./endpoints/Ticket/TicketRoutes"
 
 router.use('/payment', PaymentRoutes);
 router.use('/events',EventRoutes)
 router.use('/users', UserRoutes);
 router.use('/event-locations', EventLocationRoutes);
+router.use('/ticket', TicketRoutes);
 
 
 export default router;
