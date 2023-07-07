eb8b6f9fa6cdd1aba2bfe444aea5c9a31012d72e
Leo <s80387@beuth-hochschule.de>
2023-07-08 01:28:00 +0200

    fix cors3

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,8 @@ app.use(cors());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    req.header("Access-Control-Allow-Origin");
+    req.header("Access-Control-Allow-Headers");
     next();
 });
 
