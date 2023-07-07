43337532a74eb4af322177df25eb2dae4c821a49
Leo <s80387@beuth-hochschule.de>
2023-07-08 01:16:44 +0200

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -21,10 +21,12 @@ const swaggerFile = require('../swagger_output.json');
 dotenv.config();
 const app = express();
 app.use(cors());
-// allow same origin for development
-app.use(cors({
-    origin: 'https://mapmytickets.de'
-}));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json({limit: '50mb'}));
 // @ts-ignore
 app.use(express.json());
